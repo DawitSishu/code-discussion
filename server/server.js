00e3db689ad8423df45ae7aafe8a45388d3e0e7a
DawitSishu <dawitalemayhu55@gmail.com>
2023-05-26 12:58:55 -0400

    fixed cors again...

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const server = require('http').createServer(app);
 
 const io = require('socket.io')(server,{
     cors: {
-      origin: "http://localhost:3000" 
+      origin: "https://code-discussion.vercel.app/" 
     }
   })
 
